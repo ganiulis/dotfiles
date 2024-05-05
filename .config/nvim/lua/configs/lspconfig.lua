78d154b8b3b9fdb0f492e7daefb73663b8ceed40
Gintaras Aniulis <gintarasa95@gmail.com>
2024-05-05 20:45:50 +0300

    Update Gopls config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,10 +1,29 @@
 local lspconfig = require "lspconfig"
 local nvchad_lspconfig = require "nvchad.configs.lspconfig"
 
-for _, lsp in ipairs { "html", "cssls", "gopls" } do
+for _, lsp in ipairs { "html", "cssls" } do
   lspconfig[lsp].setup {
     on_attach = nvchad_lspconfig.on_attach,
     on_init = nvchad_lspconfig.on_init,
     capabilities = nvchad_lspconfig.capabilities,
   }
 end
+
+lspconfig.gopls.setup {
+  on_attach = nvchad_lspconfig.on_attach,
+  on_init = nvchad_lspconfig.on_init,
+  capabilities = nvchad_lspconfig.capabilities,
+  settings = {
+    gopls = {
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}

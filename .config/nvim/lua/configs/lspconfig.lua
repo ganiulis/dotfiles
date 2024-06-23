446e11b165ca3aafb824e4b2bdc2ec7f185a9b73
Gintaras Aniulis <gintarasa95@gmail.com>
2024-06-23 22:06:32 +0300

    chore: Update gopls config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -13,7 +13,7 @@ lspconfig.gopls.setup {
   on_attach = nvchad_lspconfig.on_attach,
   on_init = nvchad_lspconfig.on_init,
   capabilities = nvchad_lspconfig.capabilities,
-  settings = {
+  settings = { -- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md.
     gopls = {
       hints = {
         assignVariableTypes = true,
@@ -25,9 +25,13 @@ lspconfig.gopls.setup {
         rangeVariableTypes = true,
       },
       analyses = {
-        unusedparams = true,
+        fieldalignment = true,
+        shadow = true,
+        unusedvariable = true,
+        useany = true,
       },
       staticcheck = true,
+      vulncheck = "Imports",
       gofumpt = true,
     },
   },
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -42,7 +42,7 @@ return {
     },
   },
   {
-    "ray-x/go.nvim", -- See https://github.com/ray-x/go.nvim
+    "ray-x/go.nvim", -- See https://github.com/ray-x/go.nvim.
     dependencies = {
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",

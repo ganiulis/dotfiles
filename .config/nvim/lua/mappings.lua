c78a1696e0b298ca409ec9e58c7a40ea908b2949
Gintaras Aniulis <gintarasa95@gmail.com>
2024-07-03 20:11:48 +0300

    chore: Update Neovim config

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,21 +3,14 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
-map("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
-  desc = "Toggle Spectre",
-})
-
+map("i", "jk", "<ESC>", { desc = "Escape insert more" })
+map("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
 map("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
   desc = "Search current word",
 })
-
-map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-  desc = "Search current word",
-})
-
+map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
 map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = "Search on current file",
 })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

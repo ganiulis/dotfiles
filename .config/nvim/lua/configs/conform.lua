553cc54b92c2a5387432f00ab33cc93974ffea02
Gintaras Aniulis <gintarasa95@gmail.com>
2024-09-01 10:24:28 +0300

    feat: LSPs

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,16 +1,17 @@
-local options = {
+-- See https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters.
+require("conform").setup {
   formatters_by_ft = {
     lua = { "stylua" },
-    go = { "gofumpt" },
+    go = { "gofumpt", "goimports" },
     css = { "prettier" },
     scss = { "prettier" },
     html = { "prettier" },
-    markdown = { "prettier" },
+    markdown = { "prettier", "markdownfmt" },
+    rust = { "rustfmt" },
+    typescript = { "prettier" },
   },
   format_on_save = {
     timeout_ms = 800,
     lsp_fallback = true,
   },
 }
-
-require("conform").setup(options)
--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,9 @@
 local lspconfig = require "lspconfig"
 local nvchad_lspconfig = require "nvchad.configs.lspconfig"
 
+-- LSPs with default configurations.
 -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md.
-
-for _, lsp in ipairs { "templ", "marksman", "bashls", "tsserver", "cssls", "yamlls" } do
+for _, lsp in ipairs { "marksman", "bashls", "yamlls", "rust_analyzer" } do
   lspconfig[lsp].setup {
     on_attach = nvchad_lspconfig.on_attach,
     on_init = nvchad_lspconfig.on_init,
@@ -11,11 +11,23 @@ for _, lsp in ipairs { "templ", "marksman", "bashls", "tsserver", "cssls", "yaml
   }
 end
 
+-- TypeScript
+-- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
+-- and https://github.com/typescript-language-server/typescript-language-server.
+lspconfig.tsserver.setup {
+  on_attach = nvchad_lspconfig.on_attach,
+  on_init = nvchad_lspconfig.on_init,
+  capabilities = nvchad_lspconfig.capabilities,
+  -- See https://github.com/typescript-language-server/typescript-language-server/blob/master/docs/configuration.md.
+  settings = {},
+}
+
 lspconfig.gopls.setup {
   on_attach = nvchad_lspconfig.on_attach,
   on_init = nvchad_lspconfig.on_init,
   capabilities = nvchad_lspconfig.capabilities,
-  settings = { -- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md.
+  -- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md.
+  settings = {
     gopls = {
       hints = {
         assignVariableTypes = true,

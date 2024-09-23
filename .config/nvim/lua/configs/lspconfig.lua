2c2366260b87ea87870b6beab16d0dbb89777248
Gintaras Aniulis <gintarasa95@gmail.com>
2024-09-23 13:41:41 +0300

    Update Neovim LSP config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local nvchad_lspconfig = require "nvchad.configs.lspconfig"
 
 -- LSPs with default configurations.
 -- See https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md.
-for _, lsp in ipairs { "marksman", "bashls", "rust_analyzer" } do
+for _, lsp in ipairs { "marksman", "bashls", "rust_analyzer", "ts_ls" } do
   lspconfig[lsp].setup {
     on_attach = nvchad_lspconfig.on_attach,
     on_init = nvchad_lspconfig.on_init,

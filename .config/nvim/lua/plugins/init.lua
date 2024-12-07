1fea17f7b3a98bce7a2958b3c7e0a557fa6ee8cc
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-07 22:36:49 +0200

    Update config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -54,13 +54,6 @@ return {
     lazy = false,
   },
   {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
-    ft = { 'markdown' },
-  },
-  {
     'smoka7/multicursors.nvim',
     event = 'VeryLazy',
     dependencies = {

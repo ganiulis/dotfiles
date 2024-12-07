8f5ab815ddd360ac13bcb563e56a47b8195e5f91
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-07 23:09:04 +0200

    Update config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -7,7 +7,7 @@ local M = {}
 
 M.base46 = {
 	theme = 'chadracula-evondev',
-	theme_toggle = { 'chadracula-evondev', 'nord' },
+	theme_toggle = { 'chadracula-evondev', 'gruvbox_light' },
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },

a1e6d9d9e957ce4aa74e558b34e13180d3dc83fd
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-07 22:28:26 +0200

    Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,14 @@ vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " " -- Leader key for actions—the space bar
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
-vim.opt.rtp:prepend(lazypath)
-vim.opt.relativenumber = true -- Relative line numbers
+local option = vim.opt
+option.rtp:prepend(lazypath)
+option.relativenumber = true -- Relative line numbers
 
 require("lazy").setup({
   {
@@ -28,8 +28,9 @@ require("lazy").setup({
   },
 }, require "configs.lazy")
 
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
+local base46_cache = vim.g.base46_cache
+dofile(base46_cache .. "defaults")
+dofile(base46_cache .. "statusline")
 
 for _, file in ipairs {
   "nvchad.autocmds",

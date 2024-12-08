92ccfe03c9327f2386b1a45ab8896022bba003c7
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-08 13:21:41 +0200

    Update config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -62,43 +62,39 @@ return {
 			local set = vim.keymap.set
 
 			-- Add or skip cursor above/below the main cursor.
-			set({ 'n', 'v' }, '<up>', function() mc.lineAddCursor(-1) end)
-			set({ 'n', 'v' }, '<down>', function() mc.lineAddCursor(1) end)
-			set({ 'n', 'v' }, '<leader><up>', function() mc.lineSkipCursor(-1) end)
-			set({ 'n', 'v' }, '<leader><down>', function() mc.lineSkipCursor(1) end)
+			set({ 'n', 'v' }, '<leader>k', function() mc.lineAddCursor(-1) end)
+			set({ 'n', 'v' }, '<leader>j', function() mc.lineAddCursor(1) end)
+			-- set({ 'n', 'v' }, '<leader><up>', function() mc.lineSkipCursor(-1) end)
+			-- set({ 'n', 'v' }, '<leader><down>', function() mc.lineSkipCursor(1) end)
 
 			-- Add or skip adding a new cursor by matching word/selection
-			set({ 'n', 'v' }, '<leader>n', function() mc.matchAddCursor(1) end)
-			set({ 'n', 'v' }, '<leader>s', function() mc.matchSkipCursor(1) end)
-			set({ 'n', 'v' }, '<leader>N', function() mc.matchAddCursor(-1) end)
-			set({ 'n', 'v' }, '<leader>S', function() mc.matchSkipCursor(-1) end)
+			-- set({ 'n', 'v' }, '<leader>n', function() mc.matchAddCursor(1) end)
+			-- set({ 'n', 'v' }, '<leader>s', function() mc.matchSkipCursor(1) end)
+			-- set({ 'n', 'v' }, '<leader>N', function() mc.matchAddCursor(-1) end)
+			-- set({ 'n', 'v' }, '<leader>S', function() mc.matchSkipCursor(-1) end)
 
 			-- Add all matches in the document
-			set({ 'n', 'v' }, '<leader>A', mc.matchAllAddCursors)
+			-- set({ 'n', 'v' }, '<leader>A', mc.matchAllAddCursors)
 
 			-- You can also add cursors with any motion you prefer:
-			-- set("n", "<right>", function()
-			--     mc.addCursor("w")
-			-- end)
-			-- set("n", "<leader><right>", function()
-			--     mc.skipCursor("w")
-			-- end)
+			-- set('n', '<right>', function() mc.addCursor('w') end)
+			-- set('n', '<leader><right>', function() mc.skipCursor('w') end)
 
 			-- Rotate the main cursor.
-			set({ 'n', 'v' }, '<left>', mc.nextCursor)
-			set({ 'n', 'v' }, '<right>', mc.prevCursor)
+			-- set({ 'n', 'v' }, '<left>', mc.nextCursor)
+			-- set({ 'n', 'v' }, '<right>', mc.prevCursor)
 
 			-- Delete the main cursor.
-			set({ 'n', 'v' }, '<leader>x', mc.deleteCursor)
+			-- set({ 'n', 'v' }, '<leader>x', mc.deleteCursor)
 
 			-- Add and remove cursors with control + left click.
 			set('n', '<c-leftmouse>', mc.handleMouse)
 
 			-- Easy way to add and remove cursors using the main cursor.
-			set({ 'n', 'v' }, '<c-q>', mc.toggleCursor)
+			-- set({ 'n', 'v' }, '<c-q>', mc.toggleCursor)
 
 			-- Clone every cursor and disable the originals.
-			set({ 'n', 'v' }, '<leader><c-q>', mc.duplicateCursors)
+			-- set({ 'n', 'v' }, '<leader><c-q>', mc.duplicateCursors)
 
 			set('n', '<esc>', function()
 				if not mc.cursorsEnabled() then
@@ -111,28 +107,28 @@ return {
 			end)
 
 			-- bring back cursors if you accidentally clear them
-			set('n', '<leader>gv', mc.restoreCursors)
+			-- set('n', '<leader>gv', mc.restoreCursors)
 
 			-- Align cursor columns.
-			set('n', '<leader>a', mc.alignCursors)
+			-- set('n', '<leader>a', mc.alignCursors)
 
 			-- Split visual selections by regex.
-			set('v', 'S', mc.splitCursors)
+			-- set('v', 'S', mc.splitCursors)
 
 			-- Append/insert for each line of visual selections.
-			set('v', 'I', mc.insertVisual)
-			set('v', 'A', mc.appendVisual)
+			-- set('v', 'I', mc.insertVisual)
+			-- set('v', 'A', mc.appendVisual)
 
 			-- match new cursors within visual selections by regex.
-			set('v', 'M', mc.matchCursors)
+			-- set('v', 'M', mc.matchCursors)
 
 			-- Rotate visual selection contents.
-			set('v', '<leader>t', function() mc.transposeCursors(1) end)
-			set('v', '<leader>T', function() mc.transposeCursors(-1) end)
+			-- set('v', '<leader>t', function() mc.transposeCursors(1) end)
+			-- set('v', '<leader>T', function() mc.transposeCursors(-1) end)
 
 			-- Jumplist support
-			set({ 'v', 'n' }, '<c-i>', mc.jumpForward)
-			set({ 'v', 'n' }, '<c-o>', mc.jumpBackward)
+			-- set({ 'v', 'n' }, '<c-i>', mc.jumpForward)
+			-- set({ 'v', 'n' }, '<c-o>', mc.jumpBackward)
 
 			-- Customize how cursors look.
 			local hl = vim.api.nvim_set_hl

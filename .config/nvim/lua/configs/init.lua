330997ca9374997686ec2d6451b256665e7609da
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-12 17:40:06 +0200

    Update configuration files

--- .config/nvim/lua/configs/init.lua
+++ .config/nvim/lua/configs/init.lua
@@ -1,5 +1,4 @@
 vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
 vim.g.mapleader = ' '
-local base46_cache = vim.g.base46_cache
-dofile(base46_cache .. 'defaults')
-dofile(base46_cache .. 'statusline')
+dofile(vim.g.base46_cache .. 'defaults')
+dofile(vim.g.base46_cache .. 'statusline')

8c5ace8e69252f3bbad9c9be1b2dac3395cc87c9
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-12 08:52:15 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,9 @@
-local data = vim.fn.stdpath('data')
-
-vim.g.base46_cache = data .. '/nvchad/base46/'
+vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
 vim.g.mapleader = ' '
-
-local lazypath = data .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({ import = 'plugins' }, require('configs.lazy'))
-
 local base46_cache = vim.g.base46_cache
 dofile(base46_cache .. 'defaults')
 dofile(base46_cache .. 'statusline')
 
-for _, file in ipairs({ 'nvchad.autocmds', 'settings' }) do
+for _, file in ipairs({ 'configs.lazy', 'nvchad.autocmds', 'configs.settings' }) do
 	require(file)
 end
--- .config/nvim/lua/settings.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.relativenumber = true -- Relative line numbers
-vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = function() require('nvim-tree.api').tree.open() end })
-vim.schedule(function()
-	require('nvchad.mappings')
-	require('mappings')
-end)

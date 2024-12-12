5b31cbf81b37dc5c915229a801686fcf4cb72e7f
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-12 17:48:03 +0200

    Update configuration files

--- .config/nvim/lua/configs/mappings.lua
+++ .config/nvim/lua/configs/mappings.lua
@@ -8,16 +8,14 @@ set('i', 'jk', '<ESC>', { desc = 'Escape insert more' })
 set('n', '<leader>gp', function() vim.api.nvim_exec2('!git push', {}) end, { desc = 'push changes', silent = true })
 set('n', '<leader>gl', function() vim.api.nvim_exec2('!git pull', {}) end, { desc = 'pull changes', silent = true })
 set('n', '<leader>gcn', function()
-	local api = vim.api
-	api.nvim_exec2('!git add .', {})
-	api.nvim_exec2('!git commit -m "Add notes"', {})
-	api.nvim_exec2('!git push', {})
+	for _, cmd in ipairs({ '!git add .', '!git commit -m "Add notes"', '!git push' }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
 end, { desc = 'push note changes', silent = true })
 set('n', '<leader>gcc', function()
-	local api = vim.api
-	api.nvim_exec2('!git add .', {})
-	api.nvim_exec2('!git commit -m "Update configuration files"', {})
-	api.nvim_exec2('!git push', {})
+	for _, cmd in ipairs({ '!git add .', '!git commit -m "Update configuration files"', '!git push' }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
 end, { desc = 'push config changes', silent = true })
 
 -- Spectre
--- .config/nvim/lua/configs/settings.lua
+++ .config/nvim/lua/configs/settings.lua
@@ -1,5 +1,7 @@
 vim.opt.relativenumber = true
+
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = function() require('nvim-tree.api').tree.open() end })
+
 vim.schedule(function()
 	for _, file in ipairs({ 'nvchad.mappings', 'configs.mappings' }) do
 		require(file)

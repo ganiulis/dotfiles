05f8eb3d295ec91db1a1fa5a3d8ba79a53fd3a58
Gintaras Aniulis <gintarasa95@gmail.com>
2024-12-14 18:52:15 +0200

    Update configuration files

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,10 +1,12 @@
 return {
 	'ray-x/go.nvim',
+	event = { 'CmdlineEnter' },
 	dependencies = {
 		'ray-x/guihua.lua',
 		'neovim/nvim-lspconfig',
 		'nvim-treesitter/nvim-treesitter',
 	},
-	event = { 'CmdlineEnter' },
+	config = function() require('go').setup() end,
 	ft = { 'go', 'gomod' },
+	build = ':lua require("go.install").update_all_sync()',
 }

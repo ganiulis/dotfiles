fba1b210ddc4eddc5980f4123de1ed169ee15e4e
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-16 18:34:29 +0200

    Update configuration files

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,11 +1,6 @@
 return {
 	'ray-x/go.nvim',
 	-- event = { 'CmdlineEnter' },
-	-- dependencies = {
-	-- 	'ray-x/guihua.lua',
-	-- 	'neovim/nvim-lspconfig',
-	-- 	'nvim-treesitter/nvim-treesitter',
-	-- },
 	ft = { 'go', 'gomod' },
 	opts = {
 		-- goimports = 'gopls', -- if set to 'gopls' will use golsp format
@@ -18,8 +13,4 @@ return {
 		-- lsp_on_attach = true, -- use on_attach from go.nvim
 		-- dap_debug = true,
 	},
-	config = function(_, opts)
-		require('go').setup(opts)
-		print('go.nvim loaded.')
-	end,
 }

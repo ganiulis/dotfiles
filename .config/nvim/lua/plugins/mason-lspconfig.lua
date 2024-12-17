dae460463526a1f00a30c2ee7958892ea9ab299a
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-17 09:23:12 +0200

    Update configuration files

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,15 +1,50 @@
 return {
 	'williamboman/mason-lspconfig.nvim',
-	event = 'BufWritePre',
-	lazy = false, -- Need to check if possible to lazy-load this config
+	lazy = false,
 	config = function()
 		local lspconfig = require('lspconfig')
 		require('mason').setup()
-		local nvchad_lspconfig = require('nvchad.configs.lspconfig')
 
-		nvchad_lspconfig.defaults()
+		local on_attach = function(_, bufnr)
+			local nmap = function(keys, func, desc)
+				if desc then desc = 'LSP: ' .. desc end
+				vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+			end
+
+			nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+			nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+			nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+			nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+			nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+			nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+			nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+			nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+			-- See `:help K` for why this keymap
+			nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+			-- Lesser used LSP functionality
+			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+			nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+			nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+			nmap(
+				'<leader>wl',
+				function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+				'[W]orkspace [L]ist Folders'
+			)
+
+			vim.api.nvim_buf_create_user_command(
+				bufnr,
+				'Format',
+				function(_) vim.lsp.buf.format() end,
+				{ desc = 'Format current buffer with LSP' }
+			)
+		end
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 		require('mason-lspconfig').setup({
+			automatic_installation = true,
 			ensure_installed = {
 				'bashls',
 				'lua_ls',
@@ -21,16 +56,14 @@ return {
 			require('mason-lspconfig').setup_handlers({
 				function(server_name)
 					lspconfig[server_name].setup({
-						on_attach = nvchad_lspconfig.on_attach,
-						on_init = nvchad_lspconfig.on_init,
-						capabilities = nvchad_lspconfig.capabilities,
+						on_attach = on_attach,
+						capabilities = capabilities,
 					})
 				end,
 				['gopls'] = function()
 					lspconfig.gopls.setup({
-						on_attach = nvchad_lspconfig.on_attach,
-						on_init = nvchad_lspconfig.on_init,
-						capabilities = nvchad_lspconfig.capabilities,
+						on_attach = on_attach,
+						capabilities = capabilities,
 						settings = {
 							gopls = {
 								hints = {
@@ -51,7 +84,5 @@ return {
 				end,
 			}),
 		})
-
-		print('mason-lspconfig.nvim loaded')
 	end,
 }

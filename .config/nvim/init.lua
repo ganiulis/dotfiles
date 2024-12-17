727ea460b7e940c9eb55d2e882cde647be937af1
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-17 19:31:08 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,9 +6,8 @@ vim.o.clipboard = 'unnamedplus'
 vim.opt.relativenumber = true
 vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
 
-for _, file in ipairs({ 'defaults', 'statusline' }) do
-	dofile(vim.g.base46_cache .. file)
-end
+dofile(vim.g.base46_cache .. 'defaults')
+dofile(vim.g.base46_cache .. 'statusline')
 
 require('config.lazy')
 require('nvchad.autocmds')

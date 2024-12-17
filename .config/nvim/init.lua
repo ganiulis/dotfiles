fdeabacfb58266a7910fd47d725cd51f0dbd4615
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-17 09:15:05 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,106 @@ for _, file in ipairs({ 'defaults', 'statusline' }) do
 	dofile(vim.g.base46_cache .. file)
 end
 
-for _, file in ipairs({ 'configs.lazy', 'nvchad.autocmds', 'configs.settings' }) do
-	require(file)
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable',
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({ import = 'plugins' }, {
+	defaults = { lazy = true },
+	install = { colorscheme = { 'nvchad' } },
+	ui = {
+		icons = {
+			ft = '',
+			lazy = '󰂠 ',
+			loaded = '',
+			not_loaded = '',
+		},
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				'2html_plugin',
+				'tohtml',
+				'getscript',
+				'getscriptPlugin',
+				'gzip',
+				'logipat',
+				'netrw',
+				'netrwPlugin',
+				'netrwSettings',
+				'netrwFileHandlers',
+				'matchit',
+				'tar',
+				'tarPlugin',
+				'rrhelper',
+				'spellfile_plugin',
+				'vimball',
+				'vimballPlugin',
+				'zip',
+				'zipPlugin',
+				'tutor',
+				'rplugin',
+				'syntax',
+				'synmenu',
+				'optwin',
+				'compiler',
+				'bugreport',
+				'ftplugin',
+			},
+		},
+	},
+})
+
+require('nvchad.autocmds')
+
+vim.o.clipboard = 'unnamedplus'
+vim.opt.relativenumber = true
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = function() require('nvim-tree.api').tree.open() end })
+vim.schedule(function() require('nvchad.mappings') end)
+
+local set = vim.keymap.set
+
+-- Uncategorized mappings
+
+set('n', ';', ':', { desc = 'Enter command mode' })
+set('i', 'jk', '<ESC>', { desc = 'Escape insert more' })
+
+-- Version control
+
+set('n', 'gp', function() vim.api.nvim_exec2('!git push', {}) end, { desc = 'push changes', silent = true })
+set('n', 'gl', function() vim.api.nvim_exec2('!git pull', {}) end, { desc = 'pull changes', silent = true })
+set('n', '<leader>gcn', function()
+	for _, cmd in ipairs({ '!git add .', '!git commit -m "Add notes"', '!git push' }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
+end, { desc = 'push note changes', silent = true })
+set('n', '<leader>gcc', function()
+	for _, cmd in ipairs({ '!git add .', '!git commit -m "Update configuration files"', '!git push' }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
+end, { desc = 'push config changes', silent = true })
+
+-- Spectre
+
+set('n', '<C-s>', function() require('spectre').open_file_search() end, { desc = 'Search' })
+set(
+	'v',
+	'<C-s>',
+	function() require('spectre').open_file_search({ select_word = true }) end,
+	{ desc = 'Search current selection' }
+)
+set(
+	'n',
+	'<C-a>',
+	function() require('spectre').open_visual({ select_word = true }) end,
+	{ desc = 'Search current word' }
+)
--- .config/nvim/lua/configs/settings.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.relativenumber = true
-
-vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = function() require('nvim-tree.api').tree.open() end })
-
-vim.schedule(function()
-	for _, file in ipairs({ 'nvchad.mappings', 'configs.mappings' }) do
-		require(file)
-	end
-end)

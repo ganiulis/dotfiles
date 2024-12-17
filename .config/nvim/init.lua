e74e54b130c85143ccc74a36a14feb449bae7196
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-17 09:08:43 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,13 @@
-for _, file in ipairs({ 'configs.init', 'configs.lazy', 'nvchad.autocmds', 'configs.settings' }) do
+-- See https://github.com/nvim-lua/kickstart.nvim/blob/4b31f8ef9e37f8a02739659a0a505927114c77af/init.lua
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
+
+for _, file in ipairs({ 'defaults', 'statusline' }) do
+	dofile(vim.g.base46_cache .. file)
+end
+
+for _, file in ipairs({ 'configs.lazy', 'nvchad.autocmds', 'configs.settings' }) do
 	require(file)
 end
--- .config/nvim/lua/configs/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
-vim.g.mapleader = ' '
-
-for _, file in ipairs({ 'defaults', 'statusline' }) do
-	dofile(vim.g.base46_cache .. file)
-end

f75ad9905441012d378422809d5055dcd9c3df1a
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-17 22:03:18 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- See https://github.com/nvim-lua/kickstart.nvim/blob/4b31f8ef9e37f8a02739659a0a505927114c77af/init.lua
-require('config.setup')
+require('config.nvim')
 require('config.NvChad')
 require('config.lazy')
 require('nvchad.autocmds')

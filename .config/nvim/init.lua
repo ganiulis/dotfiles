9c999951675a233f2dffee6f43344418488678ea
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2024-12-21 12:30:02 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46/'
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 require('config.lazy')
 require('config.NvChad')
 require('config.nvim')
--- .config/nvim/lua/config/nvim.lua
+++ .config/nvim/lua/config/nvim.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 vim.o.clipboard = 'unnamedplus'
 vim.opt.relativenumber = true
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = function() require('nvim-tree.api').tree.open() end })

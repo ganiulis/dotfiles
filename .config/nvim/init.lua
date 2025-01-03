9764bfd3c0c5498b455cd33da8c76d5fe041f8a7
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-01-03 10:36:06 +0200

    Update configuration files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
-vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46/' -- Need to investigate why this fails
--- vim.g.base46_cache = vim.fn.stdpath('data') .. '/nvchad/base46/'
+vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46/'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 require('config.lazy')

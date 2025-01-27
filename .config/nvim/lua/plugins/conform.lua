74b6ebeb1988f20d353bee3579e420668667b2c6
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-01-27 08:51:21 +0200

    Add notes

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { 'stylua' },
-			go = { 'gofumpt' },
+			go = { 'gofumpt', 'goimports-reviser' },
 			css = { 'prettier' },
 			scss = { 'prettier' },
 			html = { 'prettier' },

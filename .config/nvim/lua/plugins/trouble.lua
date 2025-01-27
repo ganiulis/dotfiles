6e4cf01d1b5336e550d32e55bb7eccc6698ac86f
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-01-27 14:19:03 +0200

    Update configuration files

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -7,12 +7,12 @@ return {
 	cmd = 'Trouble',
 	keys = {
 		{
-			'<leader>xx',
+			'<leader>dx',
 			'<cmd>Trouble diagnostics toggle<cr>',
 			desc = 'Diagnostics (Trouble)',
 		},
 		{
-			'<leader>xX',
+			'<leader>dX',
 			'<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
 			desc = 'Buffer Diagnostics (Trouble)',
 		},
@@ -27,12 +27,12 @@ return {
 			desc = 'LSP Definitions / references / ... (Trouble)',
 		},
 		{
-			'<leader>xL',
+			'<leader>dL',
 			'<cmd>Trouble loclist toggle<cr>',
 			desc = 'Location List (Trouble)',
 		},
 		{
-			'<leader>xQ',
+			'<leader>dQ',
 			'<cmd>Trouble qflist toggle<cr>',
 			desc = 'Quickfix List (Trouble)',
 		},

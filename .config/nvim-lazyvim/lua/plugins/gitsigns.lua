5d299e3faf8d82e7335fd8817be0e8b99375b012
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-01 22:34:10 +0200

    Update configuration files

--- .config/nvim-lazyvim/lua/plugins/gitsigns.lua
+++ .config/nvim-lazyvim/lua/plugins/gitsigns.lua
@@ -21,7 +21,7 @@ return {
 		},
 		signs_staged_enable = true,
 		signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-		numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+		numhl = true, -- Colored numbers. Toggle with `:Gitsigns toggle_numhl`
 		linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 		word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 		watch_gitdir = {
@@ -42,7 +42,7 @@ return {
 		sign_priority = 6,
 		update_debounce = 100,
 		status_formatter = nil, -- Use default
-		max_file_length = 40000, -- Disable if file is longer than this (in lines)
+		max_file_length = 4000, -- Disable if file is longer than this (in lines)
 		preview_config = {
 			-- Options passed to nvim_open_win
 			border = "single",

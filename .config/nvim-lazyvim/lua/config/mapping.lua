e3ce264d1c65688dabd1b56d4e630fc468831cd7
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-01 21:12:22 +0200

    Update configuration files

--- .config/nvim-lazyvim/lua/config/mapping.lua
+++ .config/nvim-lazyvim/lua/config/mapping.lua
@@ -1,1 +1,11 @@
 vim.keymap.set("n", ";", ":", { desc = "Enter command mode" })
+vim.keymap.set("n", "<leader>gcn", function()
+	for _, cmd in ipairs({ "!git add .", '!git commit -m "Add notes"', "!git push" }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
+end, { desc = "push note changes" })
+vim.keymap.set("n", "<leader>gcc", function()
+	for _, cmd in ipairs({ "!git add .", '!git commit -m "Update configuration files"', "!git push" }) do
+		vim.api.nvim_exec2(cmd, {})
+	end
+end, { desc = "push config changes" })

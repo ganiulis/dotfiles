f022746d549f4976e9064f14270e827289fea755
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 13:19:08 +0200

    Update configuration

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,6 @@
 return {
+	-- Snippets for the buffer, command line, and key mappings
+	-- See https://github.com/hrsh7th/nvim-cmp.
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",

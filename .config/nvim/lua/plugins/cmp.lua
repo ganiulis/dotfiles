a245243c0b64fbd8006e60988484356db6691af9
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 13:32:53 +0200

    Update configuration

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -26,6 +26,11 @@ return {
       mapping = {
         ["<Tab>"] = cmp.mapping.select_next_item(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       },
       sources = cmp.config.sources({
         { name = "nvim_lsp" },

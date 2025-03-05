4069c8568a7bd5b81b5709090dd5ef0b3ff3d493
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 19:32:44 +0200

    Update configuration

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,40 +1,44 @@
 return {
   "folke/trouble.nvim",
   opts = {
-    auto_close = true, -- auto close when there are no items
-    auto_open = true, -- auto open when there are items
+    modes = {
+      diagnostics = {
+        auto_close = true, -- auto close when there are no items
+        auto_open = false, -- auto open when there are items
+      },
+    },
   },
   cmd = "Trouble",
   keys = {
     {
-      "<leader>dx",
+      "<C-x>",
       "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "(Trouble) Diagnostics",
+      desc = "(Trouble) Open diagnostics buffer",
     },
-    {
-      "<leader>dX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "(Trouble) Buffer Diagnostics",
-    },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "(Trouble) Symbols",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "(Trouble) LSP Definitions / references / ...",
-    },
-    {
-      "<leader>dL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "(Trouble) Location list",
-    },
-    {
-      "<leader>dQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "(Trouble) Quickfix list",
-    },
+    -- {
+    -- "<leader>dX",
+    -- "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    -- desc = "(Trouble) Buffer Diagnostics",
+    -- },
+    -- {
+    --   "<leader>cs",
+    --   "<cmd>Trouble symbols toggle focus=false<cr>",
+    --   desc = "(Trouble) Symbols",
+    -- },
+    -- {
+    --   "<leader>cl",
+    --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    --   desc = "(Trouble) LSP Definitions / references / ...",
+    -- },
+    -- {
+    --   "<leader>dL",
+    --   "<cmd>Trouble loclist toggle<cr>",
+    --   desc = "(Trouble) Location list",
+    -- },
+    -- {
+    --   "<leader>dQ",
+    --   "<cmd>Trouble qflist toggle<cr>",
+    --   desc = "(Trouble) Quickfix list",
+    -- },
   },
 }

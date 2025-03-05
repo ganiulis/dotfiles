d57f1682f44e2d71616c2dd58492909d30b9fe19
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 10:50:34 +0200

    Update configuration files

--- .config/nvim-lazyvim/lua/plugins/telescope.lua
+++ .config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
+	-- Fancy fuzzy search plugin.
+	-- See https://github.com/nvim-telescope/telescope.nvim.
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	config = function(_, opts)
 		require("telescope").setup(opts)
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 	end,
 }

bc14381f5aa9df23fd0b8c8fbedd5f0b6ad71944
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 18:26:06 +0200

    Update configuration

--- .config/nvim/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Status line
-  -- See https://github.com/nvim-lualine/lualine.nvim.
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    extensions = { "nvim-tree" },
-  },
-}
--- .config/nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd("colorscheme tokyonight-night")
-  end,
-}

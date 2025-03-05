3a16d65b60f250074b90d3304a451040ddbf0ec4
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 18:28:59 +0200

    Update configuration

--- .config/nvim/lua/plugins/wip.lua
+++ .config/nvim/lua/plugins/wip.lua
@@ -1,4 +1,5 @@
 return {
+  -- {
   -- 	-- Runs unit tests for various languages.
   -- 	-- See https://github.com/nvim-neotest/neotest.
   -- 	-- This does not work right now.
@@ -27,4 +28,32 @@ return {
   -- 			},
   -- 		})
   -- 	end,
+  -- }
+  -- {
+  -- -- Not sure if this works.
+  -- "antosha417/nvim-lsp-file-operations",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   -- Uncomment whichever supported plugin(s) you use
+  --   "nvim-tree/nvim-tree.lua",
+  --   -- "nvim-neo-tree/neo-tree.nvim",
+  --   -- "simonmclean/triptych.nvim"
+  -- },
+  -- opts = {
+  --   -- used to see debug logs in file `vim.fn.stdpath("cache") .. lsp-file-operations.log`
+  --   debug = false,
+  --   -- select which file operations to enable
+  --   operations = {
+  --     willRenameFiles = true,
+  --     didRenameFiles = true,
+  --     willCreateFiles = true,
+  --     didCreateFiles = true,
+  --     willDeleteFiles = true,
+  --     didDeleteFiles = true,
+  --   },
+  --   -- how long to wait (in milliseconds) for file rename information before cancelling
+  --   timeout_ms = 10000,
+  -- },
+  -- config = function(_, opts) require("lsp-file-operations").setup(opts) end,
+  -- }
 }

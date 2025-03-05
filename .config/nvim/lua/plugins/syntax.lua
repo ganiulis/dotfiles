1cfea64b85f75fd1571025bafefb775800a8d3e0
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-05 20:23:54 +0200

    Update configuration

--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -1,17 +1,29 @@
 return {
-  -- Advanced syntax highlighting engine.
-  -- See https://github.com/nvim-treesitter/nvim-treesitter.
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    auto_install = true,
-    ensure_installed = {
-      "lua",
-      "markdown",
-      "markdown_inline",
+  {
+    -- Advanced syntax highlighting engine.
+    -- See https://github.com/nvim-treesitter/nvim-treesitter.
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "lua",
+        "markdown",
+        "markdown_inline",
+      },
+      highlight = {
+        enable = true,
+      },
     },
-    highlight = {
-      enable = true,
-    },
+    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
   },
-  config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
+  {
+    -- Incremental selection/deselection of text.
+    -- See https://github.com/SUSTech-data/wildfire.nvim.
+    "sustech-data/wildfire.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function(_, opts) require("wildfire").setup(opts) end,
+  },
 }

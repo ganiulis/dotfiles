5688d1f76a5c1a41be88c408097b925813cf2948
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-08 23:57:10 +0200

    Update configuration

--- .config/nvim/lua/config/mapping.lua
+++ .config/nvim/lua/config/mapping.lua
@@ -1,4 +1,6 @@
 vim.keymap.set("n", ";", ":", { desc = "Enter command mode" })
+vim.keymap.set("v", "<C-_>", "gc", { remap = true })
+vim.keymap.set("n", "<C-_>", "gcc<bar>j", { remap = true })
 vim.keymap.set("n", "<leader>gpn", function()
   for _, cmd in ipairs({ "!git add .", '!git commit -m "Add notes"', "!git push" }) do
     vim.api.nvim_exec2(cmd, {})
@@ -9,5 +11,3 @@ vim.keymap.set("n", "<leader>gpc", function()
     vim.api.nvim_exec2(cmd, {})
   end
 end, { desc = "(Custom) [g]it [p]ush [c]onfig" })
-vim.keymap.set({ "v" }, "<C-_>", "gc", { remap = true })
-vim.keymap.set({ "n" }, "<C-_>", "gcc<bar>j", { remap = true })

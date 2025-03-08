593b3f81168f1f0ad8471de61c8a1eaad1500ec8
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-08 17:24:58 +0200

    Update configuration

--- .config/fish/functions/prompt/kube_toggle.fish
+++ .config/fish/functions/prompt/kube_toggle.fish
@@ -1,11 +1,21 @@
 function kube_toggle -a toggle
-  if test "$toggle" = "on"
-    set -U __kube_ps_enabled 1
-    return
-  end
+    if test "$toggle" = on
+        if not type -q kubectl
+            echo 'kubectl not found'
+            return
+        end
 
-  if test "$toggle" = "off"
-    set -U __kube_ps_enabled 0
-    return
-  end
+        if not type -q python3
+            echo 'python3 not found'
+            return
+        end
+
+        set -U __kube_ps_enabled 1
+        return
+    end
+
+    if test "$toggle" = off
+        set -U __kube_ps_enabled 0
+        return
+    end
 end

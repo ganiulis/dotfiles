735822e22d9c8fc66c3846f8b868053ff0b52bac
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-08 15:56:17 +0200

    Update configuration

--- .config/fish/functions/dotfiles/dotfiles_check_health.fish
+++ .config/fish/functions/dotfiles/dotfiles_check_health.fish
@@ -12,7 +12,13 @@ function dotfiles_check_health --description 'Dotfiles health check'
         'nvim      (Neovim text editor)             Visit https://neovim.io'
 
     echo
-    echo 'Programming languages'
+    echo 'Programming language related'
+    internal_check_for_cmd clang \
+        'clang     (C compiler alternative)         ' \
+        'clang     (C compiler alternative)         Visit https://clang.llvm.org'
+    internal_check_for_cmd gcc \
+        'gcc       (C compiler)                     ' \
+        'gcc       (C compiler)                     Visit https://gcc.gnu.org'
     internal_check_for_cmd go \
         'go        (Programming language)           ' \
         'go        (Programming language)           Visit https://go.dev' \

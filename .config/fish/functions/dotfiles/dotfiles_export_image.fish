c50d217a8e5a6fb33011fbb76b3b82c199f3b65a
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-10 22:40:01 +0200

    Update configuration

---
+++ .config/fish/functions/dotfiles/dotfiles_export_image.fish
@@ -0,0 +1,12 @@
+function dotfiles_export_image -d 'Build and export an image'
+    if not count $argv >/dev/null
+        echo 'Missing argument'
+        return
+    end
+
+    set -f image $argv[1]
+
+    docker run -t --name img_export $image ls /
+    docker export img_export >img.tar
+    docker rm img_export
+end

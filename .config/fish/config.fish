8b8326b6247ca4159c305f9d5181c579e69088ba
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-15 23:08:35 +0200

    Update configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,3 @@ if status is-interactive
     set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
     set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 end
-
-# Sets GOPATH env variable when go directory exists
-if test -d ~/.local/go
-    set -Ux GOPATH ~/.local/go
-end

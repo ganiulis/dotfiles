aa57586e042f58d81e5b3ba2c6d5c38b497b7729
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-19 09:31:39 +0200

    Update configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
     set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
     set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 end
+
+# Sets GOPATH if go directory is moved to local
+if test -d ~/.local/go
+    set -gx GOPATH ~/.local/go
+end
